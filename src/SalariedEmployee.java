5c3bf2fedafea3215743a0f328cadfd82a00cf3e
hakenshi <fkdias025@gmail.com>
2024-09-02 20:49:38 -0300

    first commit

---
+++ src/SalariedEmployee.java
@@ -0,0 +1,29 @@
+public class SalariedEmployee  extends Employee{
+
+    private double weeklySalary;
+
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, double weeklySalary) {
+        super(firstName, lastName, socialSecurityNumber);
+        this.weeklySalary = weeklySalary;
+    }
+
+
+    @Override
+    public double earnings() {
+        return 0;
+    }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    public void setWeeklySalary(double weeklySalary) {
+        this.weeklySalary = weeklySalary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n" + "Weekly Salary: " + getWeeklySalary();
+    }
+
+}
